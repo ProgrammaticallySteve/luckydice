ad996fcb0fd0d4ebd785bacc744308fc3bf5e2b8
Stephen Harvey <itsteveharvey@gmail.com>
2022-09-13 15:47:55 +1200

    Set attribute for image2 (right die) and apply randomImageSource2

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
+// Left Die
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // Gives random number between 1 and 6
 
-var randDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage;
 
-document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+
+// Right Die
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
